003ddb5cb36d1b2c6507eb081a34b1f6d3ec34cf
Onur-Yılmaz <onuryilmaz@trair.com.tr>
2024-03-05 17:02:55 +0300

    Fix validation errors and import order in models.py

--- appointment/models.py
+++ appointment/models.py
@@ -1,9 +1,9 @@
-from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-import datetime
 from counselor.models import ConsultantProfile
+import datetime
+from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
 class WorkDay(models.Model):
     counselor = models.ForeignKey(ConsultantProfile, related_name='work_day_ranges', on_delete=models.CASCADE)
@@ -18,14 +18,19 @@ class WorkDay(models.Model):
         return f"{self.counselor.name} - {self.start_date} - {self.end_date}"
 
     def create_sessions(self):
+        if self.start_date > self.end_date:
+            raise ValidationError(_("Start date cannot be after end date."))
+
+        if self.start_time >= self.end_time:
+            raise ValidationError(_("Start time cannot be after or equal to end time."))
 
         days_count = (self.end_date - self.start_date).days + 1
         for day in range(days_count):
             work_day = self.start_date + datetime.timedelta(days=day)
+            if work_day > self.end_date:
+                break  # Son tarihden sonra çalışmayı durdur
             start_time = datetime.datetime.combine(work_day, self.start_time)
             end_time = datetime.datetime.combine(work_day, self.end_time)
-
-
             session_duration = datetime.timedelta(minutes=self.session_duration)
             break_duration = datetime.timedelta(minutes=self.break_duration)
 
@@ -47,4 +52,3 @@ class Appointment(models.Model):
 
     def __str__(self):
         return f"{self.counselor.name} - {self.work_day} - {self.start_time} - {self.end_time}"
-
